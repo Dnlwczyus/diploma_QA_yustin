f321137535dd7f9774a0c4a7d60f62e1f3e7db8f
Dnlwczyus <134511390+Dnlwczyus@users.noreply.github.com>
2023-06-19 16:31:39 +0300

    Delete configuration.py

--- configuration.py
+++
@@ -1,5 +0,0 @@
-# Данилович Юстин, 5-я когорта — Финальный проект. Инженер по тестированию плюс
-
-URL = "https://96087f54-3b29-4c0d-9515-1be6ea6bea0b.serverhub.praktikum-services.ru"
-CREATE_ORDER = '/api/v1/orders'
-TRACK_ORDER = '/v1/orders/track?t='
